53768385b7341638cf4b57f567b37e96e85db7a0
Venkata Pingali <pingali@gmail.com>
2011-11-30 14:25:30 +0530

    Updated forms

--- django_auth_aadhaar/forms.py
+++ django_auth_aadhaar/forms.py
@@ -1,10 +1,8 @@
 from django import forms
 
 class AadhaarAuthForm(forms.Form):
-    GENDER_CHOICES=(
-        ('M', 'Male'), 
-        ('F', 'Female'),
-        ('T', 'Transgender'))
+    
+    # Required fields. 
     ATTRIBUTE_CHOICES=(
         ("aadhaar_name", "Name"),
         ("aadhaar_dob", "Date of Birth"),
@@ -17,8 +15,20 @@ class AadhaarAuthForm(forms.Form):
         forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple, 
                                   label="Authentication Parameters",
                                   choices=ATTRIBUTE_CHOICES,
-                                  help_text="Please select one or more of attributes you wish to authenticate"
-                                           )
+                                  help_text="Please select one or more of attributes you wish to authenticate")
+
+    # Personally identifiable information
+    PI_MATCH_CHOICES =(
+        ('E', 'Exact'), 
+        ('P', 'Partial')
+        )
+    GENDER_CHOICES=(
+        ('M', 'Male'), 
+        ('F', 'Female'),
+        ('T', 'Transgender'))
+    aadhaar_match = forms.ChoiceField(choices=PI_MATCH_CHOICES,
+                                      label="Match",
+                                      required=False)
     aadhaar_name = forms.CharField(max_length=120, label="Name",
                                    required=False)
     aadhaar_gender = forms.ChoiceField(choices=GENDER_CHOICES,
@@ -26,28 +36,43 @@ class AadhaarAuthForm(forms.Form):
                                        required=False)
     aadhaar_dob = forms.DateField(label="DoB", 
                                   required=False,
-                                  #help_text="Date of Birth (format: YYYY or YYYY-MM-DD)",
+                                  help_text="Date of Birth (format: YYYY or YYYY-MM-DD)",
                                   input_formats=['%Y', '%Y-%m-%d'])
     aadhaar_email = forms.EmailField(max_length=64, label="Email", 
                                      required=False)
-    aadhaar_landmark = forms.CharField(max_length=120, label="Landmark", 
-                                       default="", required=False)
+    aadhaar_phone = forms.IntegerField(max_length=10, label="Phone", 
+                                     required=False)
+    
+    # => Address 
+    #<Pa ms="E" co="" house="" street="" lm="" loc=""
+    # vtc="" subdist="" dist="" state="" pc="" po=""/>
+    aadhaar_co = forms.CharField(max_length=120, label="Careof", 
+                                 default="", required=False)
+    aadhaar_house = forms.CharField(max_length=120, label="House", 
+                                     default="",   required=False)
     aadhaar_street = forms.CharField(max_length=120, label="Street", 
                                      default="",   required=False)
+    aadhaar_landmark = forms.CharField(max_length=120, label="Landmark", 
+                                       default="", required=False)
     aadhaar_locality = forms.CharField(max_length=120, label="Locality", 
-                                     default="",   required=False)
+                                       default="",   required=False)
     aadhaar_vtc = forms.CharField(max_length=40, label="VTC", 
-                                     default="",   required=False)
+                                  default="",   required=False)
     aadhaar_subdist = forms.CharField(max_length=40, label="Sub-District", 
-                                     default="",   required=False)    
+                                      default="",   required=False)    
     aadhaar_district= forms.CharField(max_length=40, label="District", 
-                                     default="",   required=False)    
+                                      default="",   required=False)    
     aadhaar_state = forms.CharField(max_length=140, label="State", 
-                                     default="",   required=False)
+                                    default="",   required=False)
     aadhaar_pincode = forms.IntegerField(max_length=6, label="State", 
                                      default=0,   required=False)
-    
-    def clean_aadhaar_pincode(self): 
+    aadhaar_postoffice = forms.CharField(max_length=40, label="PostOffice", 
+                                         default="",   required=False)
+    def clean(self):
+        
+        attributes = self.cleaned_data['aadhaar_attributes']
+        # Do something based on the attribtued 
+
         pincode = self.cleaned_data['aadhaar_pincode']
         if ((pincode != 0) and (pincode < 100000)):
             raise forms.ValidationError("Pincode, if specified, " + 
