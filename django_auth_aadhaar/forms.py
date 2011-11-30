c779d860fb994365f034278d10517b402fce24fb
Venkata Pingali <pingali@gmail.com>
2011-11-30 04:52:29 +0000

    Continuing to expand the form

--- django_auth_aadhaar/forms.py
+++ django_auth_aadhaar/forms.py
@@ -30,6 +30,25 @@ class AadhaarAuthForm(forms.Form):
                                   input_formats=['%Y', '%Y-%m-%d'])
     aadhaar_email = forms.EmailField(max_length=64, label="Email", 
                                      required=False)
+    aadhaar_landmark = forms.CharField(max_length=120, label="Landmark", 
+                                       default="", required=False)
     aadhaar_street = forms.CharField(max_length=120, label="Street", 
-                                     required=False)
+                                     default="",   required=False)
+    aadhaar_locality = forms.CharField(max_length=120, label="Locality", 
+                                     default="",   required=False)
+    aadhaar_vtc = forms.CharField(max_length=40, label="VTC", 
+                                     default="",   required=False)
+    aadhaar_subdist = forms.CharField(max_length=40, label="Sub-District", 
+                                     default="",   required=False)    
+    aadhaar_district= forms.CharField(max_length=40, label="District", 
+                                     default="",   required=False)    
+    aadhaar_state = forms.CharField(max_length=140, label="State", 
+                                     default="",   required=False)
+    aadhaar_pincode = forms.IntegerField(max_length=6, label="State", 
+                                     default=0,   required=False)
     
+    def clean_aadhaar_pincode(self): 
+        pincode = self.cleaned_data['aadhaar_pincode']
+        if ((pincode != 0) and (pincode < 100000)):
+            raise forms.ValidationError("Pincode, if specified, " + 
+                                        "should be > 100000")
